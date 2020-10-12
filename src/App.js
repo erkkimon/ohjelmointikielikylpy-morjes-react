9f3655d925deef7eacbeb0c1cfc84e51dc9f7e56
Tom Himanen <tom.himanen@gmail.com>
2020-10-12 10:59:39 +0300

    Implements adding new tasks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
 function App() {
+
+  const [taskList, setTaskList] = useState(["asdf", "qwer", "zxcv", "hjkl"])
+  const [newTaskItem, updateNewTaskItem] = useState("")
+
+  // function addNewTaskToList () {
+  //   alert("heippa vanha tapa")
+  // }
+
+  let addNewTaskToList = function(taskItem) {
+    let taskListCopy = [...taskList]
+    taskListCopy.push(taskItem)
+    setTaskList(taskListCopy)
+  }
+
+  // useState() ---------->>>>>>>> [counter = 0, increaseCounter = useState()]
+
+    //super(props)
+    // this.state = {
+      // taskList: [
+      //   "Tehtävä 1",
+      //   "Tehtävä 2",
+      //   "Tehtävä 3"
+      // ]
+    // }
+
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Reactin kilpailuedun
-        </a>
-      </header>
+
+      <input type="text" value={newTaskItem} onChange={ (e) => updateNewTaskItem(e.target.value) } />
+      <input type="button" value="Lisää taski" onClick={() => addNewTaskToList(newTaskItem) } />
+
+      { taskList.map( (singleTask) => <p>Tehtävän sisältö: {singleTask}</p>) }
+
+      {/* <button type="button" onClick={() => increaseCounter(counter + 1)}>Laskurin arvo {counter}</button> */}
     </div>
   );
+
 }
 
 export default App;
