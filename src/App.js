fd62ddfdbeab9ef0f038e4f14f2fac5c83110e65
Tom Himanen <tom.himanen@gmail.com>
2020-10-13 09:42:40 +0300

    Implements deletion of tasks

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import React, { useEffect, useState } from "react";
 
 function App() {
 
-  const [taskList, setTaskList] = useState([
-    { "id": "8324587", "taskContent": "asdf" }, 
-    { "id": "9236495", "taskContent": "qwer" },
-    { "id": "3456754", "taskContent": "zxcv" }
-  ])
+  const [taskList, setTaskList] = useState([])
   const [newTaskItem, updateNewTaskItem] = useState("")
-
-  // function addNewTaskToList () {
-  //   alert("heippa vanha tapa")
-  // }
 
   let addNewTaskToList = function(taskItem) {
     let taskListCopy = [...taskList]
-    taskListCopy.push(taskItem)
+    let taskItemObj = { "id": Math.floor(Date.now() / 1000), "taskContent": taskItem }
+    taskListCopy.push(taskItemObj)
     setTaskList(taskListCopy)
-    console.log(taskList)
+    updateNewTaskItem("")
+    //console.log(taskList)
   }
 
-  // useState() ---------->>>>>>>> [counter = 0, increaseCounter = useState()]
-
-    //super(props)
-    // this.state = {
-      // taskList: [
-      //   "Tehtävä 1",
-      //   "Tehtävä 2",
-      //   "Tehtävä 3"
-      // ]
-    // }
+  let deleteTask = function(taskId) {
+    let taskListCopy = [...taskList]
+    // let taskListAnotherCopy = []
+    // taskListCopy.forEach((taskListItem) => if(taskListItem.id === taskId { taskListAnotherCopy.push(taskListItem)}))
+    let shortenedTaskList = taskListCopy.filter(taskListItem => taskListItem.id !== taskId)
+    //console.log(shortenedTaskList)
+    setTaskList(shortenedTaskList)
+  }
 
   return (
 
@@ -38,7 +30,7 @@ function App() {
       <input type="text" value={newTaskItem} onChange={ (e) => updateNewTaskItem(e.target.value) } />
       <input type="button" value="Lisää taski" onClick={() => addNewTaskToList(newTaskItem) } />
 
-      { taskList.map( (singleTask) => <p>{singleTask.taskContent}</p>) }
+      { taskList.map( (singleTask) => <p>{singleTask.taskContent} <button onClick={() => deleteTask(singleTask.id)}>x</button></p>) }
 
       {/* <button type="button" onClick={() => increaseCounter(counter + 1)}>Laskurin arvo {counter}</button> */}
     </div>
