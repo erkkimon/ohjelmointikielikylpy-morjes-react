0689a150b0099ff14229b43350e02d301c4230b0
Tom Himanen <tom.himanen@gmail.com>
2020-10-13 07:49:11 +0300

    Checkpoint 13.10.2020

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useEffect, useState } from "react";
 
 function App() {
 
-  const [taskList, setTaskList] = useState(["asdf", "qwer", "zxcv", "hjkl"])
+  const [taskList, setTaskList] = useState([
+    { "id": "8324587", "taskContent": "asdf" }, 
+    { "id": "9236495", "taskContent": "qwer" },
+    { "id": "3456754", "taskContent": "zxcv" }
+  ])
   const [newTaskItem, updateNewTaskItem] = useState("")
 
   // function addNewTaskToList () {
@@ -13,6 +17,7 @@ function App() {
     let taskListCopy = [...taskList]
     taskListCopy.push(taskItem)
     setTaskList(taskListCopy)
+    console.log(taskList)
   }
 
   // useState() ---------->>>>>>>> [counter = 0, increaseCounter = useState()]
@@ -33,7 +38,7 @@ function App() {
       <input type="text" value={newTaskItem} onChange={ (e) => updateNewTaskItem(e.target.value) } />
       <input type="button" value="Lisää taski" onClick={() => addNewTaskToList(newTaskItem) } />
 
-      { taskList.map( (singleTask) => <p>Tehtävän sisältö: {singleTask}</p>) }
+      { taskList.map( (singleTask) => <p>{singleTask.taskContent}</p>) }
 
       {/* <button type="button" onClick={() => increaseCounter(counter + 1)}>Laskurin arvo {counter}</button> */}
     </div>
